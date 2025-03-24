750510a3fbf7f8af2468025625f484d055aa87c3
wlfrd18 <wilfriedpanol@gmail.com>
2025-03-24 16:16:04 +0000

    add all the other tasks

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,6 @@
+export default function appendToEachArrayValue(array, appendString) {
+    for (let [idx, value] of array.entries()) {
+      array[idx] = appendString + value;
+    }
+    return array;
+  }
