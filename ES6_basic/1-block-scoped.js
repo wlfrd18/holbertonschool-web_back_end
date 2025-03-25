49dde6be47ba1f246ac1f02fe4c30ce268a0423c
wlfrd18 <wilfriedpanol@gmail.com>
2025-03-25 21:23:10 +0000

    delete the block trueorfalse of the task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
+export default function taskBlock() {
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
