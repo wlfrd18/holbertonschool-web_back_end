113795752090419eb9c614ba7f48f209900c4f41
wlfrd18 <wilfriedpanol@gmail.com>
2025-03-25 21:12:19 +0000

    fix my tasks by eslint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const new_array = array;
-    for (let value of array) {
-      const idx = array.indexOf(value);
-      new_array[idx] = appendString + value;
-    }
-  
-    return new_array;
+  const newArray = [...array];
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    newArray[idx] = appendString + value;
+  }
+
+  return newArray;
 }
--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,16 +1,16 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const year = getCurrentYear();
-  
-    const budget = { 
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const year = getCurrentYear();
+
+  const budget = {
     [`income-${year}`]: income,
     [`gdp-${year}`]: gdp,
     [`capita-${year}`]: capita,
-    }
-  
-    return budget;
-  }
+  };
+
+  return budget;
+}
