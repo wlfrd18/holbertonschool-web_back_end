996e7e6f40eac077bec7092447dcf181e736fb15
wlfrd18 <wilfriedpanol@gmail.com>
2025-04-25 13:20:16 +0000

    save 2 files

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module serves as basic example for variable annotation
+for multiple simple types
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
