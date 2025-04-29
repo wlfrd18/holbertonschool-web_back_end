1e08cbcb823fb1c3155089f03b8caf9c8b17a079
wlfrd18 <wilfriedpanol@gmail.com>
2025-04-29 15:22:35 +0000

    add an annoted type return to the task 0 for the spend value

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,7 +11,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     Coroutine that loops 10 times.
     Each iteration waits 1 second asynchronously,
