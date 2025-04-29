38c3e41771eccab5d999a8a708f700a1aca35a73
wlfrd18 <wilfriedpanol@gmail.com>
2025-04-29 15:29:41 +0000

    change the variable of the for loop

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,12 +11,12 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Coroutine that loops 10 times.
     Each iteration waits 1 second asynchronously,
     then yields a random float between 0 and 10.
     """
-    for n in range(10):
+    for number in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
