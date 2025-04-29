c868482e616c427438209e67eb7c174c3f83fe4c
wlfrd18 <wilfriedpanol@gmail.com>
2025-04-29 15:44:04 +0000

    reverse the two instructions inside the for loop

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,6 @@
 This module contains a coroutine that asynchronously yields
 random numbers between 0 and 10, with a 1-second delay between each.
 """
-
 import asyncio
 import random
 from typing import AsyncGenerator
@@ -18,5 +17,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     then yields a random float between 0 and 10.
     """
     for number in range(10):
-        await asyncio.sleep(1)
         yield random.uniform(0, 10)
+        await asyncio.sleep(1)
