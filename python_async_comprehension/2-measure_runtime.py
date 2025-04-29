d89c60f0f2e13dd2a475f66528adcb55c528e27d
wlfrd18 <wilfriedpanol@gmail.com>
2025-04-29 15:18:26 +0000

    fix the task 2 by pycodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,13 +2,14 @@
 """
 2-measure_runtime
 
-this module contains a function that measure 
+this module contains a function that measure
 run time for 4 parralle asynchronous comprehension
 """
 
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
+
 
 async def measure_runtime() -> float:
     """
@@ -18,7 +19,7 @@ async def measure_runtime() -> float:
     tasks = [async_comprehension() for n in range(4)]
 
     await asyncio.gather(*tasks)
-    
+
     end = time.perf_counter()
 
     elapsed_time = end - start
