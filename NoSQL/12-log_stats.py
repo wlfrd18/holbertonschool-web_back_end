49dd7717ab1752092bb8a0ec1ee0464ae4f8db8a
wlfrd18 <wilfriedpanol@gmail.com>
2025-05-02 21:22:27 +0000

    set my script to be execute if __name__ = "__main__"

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,31 +1,14 @@
 #!/usr/bin/env python3
-"""
-Provides stats about Nginx logs stored in MongoDB.
-"""
-
+"""Gets stats for Nginx from  MongoDB"""
 from pymongo import MongoClient
 
 
-def log_stats():
-    """Displays statistics about Nginx logs."""
-    client = MongoClient()
-    collection = client.logs.nginx
-
-    # Total number of logs
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    # HTTP Methods count
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    # Number of GET requests to path "/status"
-    status_check = collection.count_documents({"method": "GET",
-                                               "path": "/status"})
-    print(f"{status_check} status check")
-
 if __name__ == "__main__":
-    log_stats()
+    data = MongoClient().logs.nginx
+    print(f'{data.count_documents({})} logs\nMethods:')
+    print(f'\tmethod GET: {data.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {data.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {data.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {data.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {data.count_documents({"method": "DELETE"})}')
+    print(f'{data.count_documents({"path": "/status"})} status check')
