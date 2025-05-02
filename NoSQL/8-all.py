1b5587bf615ffb2efcfbb964e032431367357e90
wlfrd18 <wilfriedpanol@gmail.com>
2025-05-02 19:32:22 +0000

    add all the tasks of this project

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+list all pymongo
+"""
+
+
+def list_all(mongo_collection):
+    """
+    lists all documents in a collection
+    """
+    documents = mongo_collection.find()
+    return documents
--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,16 +5,14 @@
 This module contains a coroutine that asynchronously yields
 random numbers between 0 and 10, with a 1-second delay between each.
 """
+from typing import AsyncGenerator
 import asyncio
 import random
-from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     Coroutine that loops 10 times.
-    Each iteration waits 1 second asynchronously,
-    then yields a random float between 0 and 10.
     """
     for number in range(10):
         yield random.uniform(0, 10)
