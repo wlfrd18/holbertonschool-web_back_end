906f7077f18218091fad405a73ea4843b0da3900
wlfrd18 <wilfriedpanol@gmail.com>
2025-05-02 21:28:42 +0000

    push for test the checker

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python3
-"""Gets stats for Nginx from  MongoDB"""
+"""
+Provides some stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
+from typing import List, Dict, Optional
 
 
-if __name__ == "__main__":
-    data = MongoClient().logs.nginx
-    print(f'{data.count_documents({})} logs\nMethods:')
-    print(f'\tmethod GET: {data.count_documents({"method": "GET"})}')
-    print(f'\tmethod POST: {data.count_documents({"method": "POST"})}')
-    print(f'\tmethod PUT: {data.count_documents({"method": "PUT"})}')
-    print(f'\tmethod PATCH: {data.count_documents({"method": "PATCH"})}')
-    print(f'\tmethod DELETE: {data.count_documents({"method": "DELETE"})}')
-    print(f'{data.count_documents({"path": "/status"})} status check')
+def count(collection, options: Dict[str, str] = {}) -> int:
+    """
+    Take a mongoDB collection, filter data and count it
+
+    Args:
+        collection: MongoDB collection
+        options (Dict[str, str]): options to filter
+    """
+    return collection.count_documents(options)
+
+
+if __name__ == '__main__':
+    client = MongoClient(host="localhost", port=27017)
+    collection = client.logs.nginx
+    methods: List[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("{:d} logs".format(count(collection)))
+    print("Methods:")
+
+    for method in methods:
+        print("\tmethod {:s}: {:d}".format(
+            method,
+            count(collection, {"method": method})
+        ))
+
+    print("{:d} status check".format(
+        count(collection, {"method": "GET", "path": "/status"})
+    ))
